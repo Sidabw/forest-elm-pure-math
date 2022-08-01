dd5ebdb654840fba6ff5b6dc382efcf1adad8029
shaogz <shaogz@bokecc.com>
2022-08-01 16:52:33 +0800

    八皇后所有解法

--- src/com/brew/home/geekbang/recall/Queue8_1_FindFirst.java
+++ src/com/brew/home/geekbang/recall/Queue8_1_FindFirst.java
@@ -1,17 +1,24 @@
-package com.brew.home.geekbang;
+package com.brew.home.geekbang.recall;
 
 /**
  * 八皇后问题，程序员小灰
+ * <p> 1. 回溯本身就是暴力枚举，但是重要在于"在匹配失败的情况下提前结束（剪枝），回溯到上一层继续"
  * https://mp.weixin.qq.com/s/puk7IAZkSe6FCkZnt0jnSA
  */
-public class Queue8 {
+public class Queue8_1_FindFirst {
+
+    public static void main(String[] args) {
+        Queue8_1_FindFirst queue8 = new Queue8_1_FindFirst();
+        queue8.settleQueen(0);
+        queue8.printChessBoard();
+    }
 
     static final int MAX_NUM = 8;
 
-    int[][] chessBoard = new int[MAX_NUM][MAX_NUM];
+    static int[][] chessBoard = new int[MAX_NUM][MAX_NUM];
 
     //检查落点是否符合规则
-    boolean check(int x, int y) {
+    static boolean check(int x, int y) {
         for (int i = 0; i < y; i++) {
             //检查纵向
             if (chessBoard[x][i] == 1) {
@@ -29,12 +36,12 @@ public class Queue8 {
         return true;
     }
 
-    boolean settleQueen(int y) {
+    static boolean settleQueen(int y) {
         //行数超过8，说明已经找出答案
         if(y == MAX_NUM){
             return true;
         }
-        //遍历当前行，逐一格子验证
+        //遍历《当前行》，逐一格子验证
         for(int i=0;i<MAX_NUM;i++) {
             //为当前行清零，以免在回溯的时候出现脏数据
             for(int x=0; x<MAX_NUM; x++) {
@@ -43,17 +50,21 @@ public class Queue8 {
             //检查是否符合规则，如果符合，更改元素值并进一步递归
             if(check(i,y)){
                 chessBoard[i][y] = 1;
-                //递归如果返回true，说明下层已找到解法，无需继续循环
+                /*
+                 1. settleQueen(y+1) 递归进入下一层
+                 2. 递归如果返回true，说明下层已找到解法，无需继续循环
+                 */
                 if(settleQueen(y+1)) {
                     return true;
                 }
             }
         }
+        //如果当前行遍历完了，则return false，回溯到前一行。前一行i+1，到下一个格子
         return false;
     }
 
     //打印棋盘当前值
-    void printChessBoard(){
+    static void printChessBoard(){
         for(int j=0; j<MAX_NUM; j++) {
             for (int i = 0; i < MAX_NUM; i++) {
                 System.out.print(chessBoard[i][j]);
@@ -62,10 +73,5 @@ public class Queue8 {
         }
     }
 
-    public static void main(String[] args) {
-        Queue8 queue8 = new Queue8();
-        queue8.settleQueen(0);
-        queue8.printChessBoard();
-    }
 }
 
---
+++ src/com/brew/home/geekbang/recall/Queue8_2_FindAll.java
@@ -0,0 +1,43 @@
+package com.brew.home.geekbang.recall;
+
+import static com.brew.home.geekbang.recall.Queue8_1_FindFirst.*;
+
+/**
+ * 八皇后，找到所有排列。
+ * <p> 1. 回溯本身就是暴力枚举，但是重要在于"在匹配失败的情况下提前结束（剪枝），回溯到上一层继续"。
+ * <p> 2. 找到所有排列。到第八层找到正确排列后，只输出结果，不结束。回溯到上一层/上上层.. 继续找。这么倒着继续找的原因，就是枚举所有情况，然后找到所有正确排列
+ *
+ */
+public class Queue8_2_FindAll {
+
+    static int num=0;
+    public static void main(String[] args) {
+        settleQueen(0);
+    }
+
+    static void settleQueen(int y) {
+        //行数超过8，说明已经找出答案
+        if(y == MAX_NUM){
+            printChessBoard();
+            System.out.println("<><><><><><><><><><><>got u <><><><><><><><><><><>, " + ++num);
+            return;
+        }
+        //遍历《当前行》，逐一格子验证
+        for(int i=0;i<MAX_NUM;i++) {
+            //为当前行清零，以免在回溯的时候出现脏数据
+            for(int x=0; x<MAX_NUM; x++) {
+                chessBoard[x][y] = 0;
+            }
+            //检查是否符合规则，如果符合，更改元素值并进一步递归
+            if(check(i,y)){
+                chessBoard[i][y] = 1;
+                /*
+                 1. settleQueen(y+1) 递归进入下一层
+                 2. 递归如果返回true，说明下层已找到解法，无需继续循环
+                 */
+                settleQueen(y+1);
+            }
+        }
+        //如果当前行遍历完了，则return false，回溯到前一行。前一行i+1，到下一个格子
+    }
+}
