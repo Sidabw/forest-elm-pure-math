f8d628cf48f70ace2301df698242469160a13717
shaogz <shaogz@bokecc.com>
2023-04-07 16:47:13 +0800

    提交一版超过时间限制的三数之和代码

--- src/com/brew/home/leetcode/list/LeetCode15.java
+++ src/com/brew/home/leetcode/list/LeetCode15.java
@@ -23,76 +23,59 @@ public class LeetCode15 {
         //下一个b1元素，因为b1>b0，所以如果存在c1使得a1+b1+c1=0的话，那么c1<c0
         //双重for循环，但是一重和二重并不是简单的并行关系，而是有"相互关系"此时考虑能否双指针
 
-        System.out.println(1);
-
-
+        // int[] nums = new int[]{0,0,0};
+        // int[] nums = new int[]{0,1,1};
+        int[] nums = new int[]{-1,0,1,2,-1,-4};
+        // int[] nums = new int[]{-1,-1,-1,-2,-2,1,0,0,1,3,3,3,5};
+        // int[] nums = new int[]{-1,0,1,2,-1,-4,-2,-3,3,0,4};
+        // int[] nums = new int[]{-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6};
+        System.out.println(threeSum(nums));
     }
 
 
     public static List<List<Integer>> threeSum(int[] nums) {
         new QuickSortGeekBang().quick(nums, 0, nums.length-1);
-        //费尽心机处理重复元素，打一开始排完序再把重复元素删了不久行了
-
-        LinkedList<Integer> numsSet = new LinkedList<>();
-        for (int num : nums) {
-            int lastEl;
-            try {
-                lastEl = numsSet.getLast();
-                System.out.println(1);
-            } catch (NoSuchElementException e) {
-
-            }
-            if (numsSet.getLast() != null && numsSet.getLast() == num) {
-                continue;
-            }
-            numsSet.add(num);
+        if(nums.length<3) {
+            return Collections.emptyList();
         }
 
-        System.out.println(2);
-        System.out.println(3);
+        ArrayList<List<Integer>> result = new ArrayList<List<Integer>>();
         
-        
-
-
-
-        LinkedList<List<Integer>> result = new LinkedList<List<Integer>>();
         for (int a = 0; a < nums.length - 2; a++) {
+            int for3Index = nums.length-1;    
+            for(int b = a+1; b < nums.length - 1; b++) {
 
-            Integer preBval = null;
-            for(int b = 1; b < nums.length - 1; b++) {
-                if (preBval != null && preBval == nums[b]) {
-                    continue;
-                }
-
-                
-                int for3Index;
-                if(!result.isEmpty()) {
-                    Integer c0 = result.getLast().get(2);
-                    for3Index = c0;
-                } else {
-                    for3Index = nums.length - 1;
-                }
-
-                Integer preCval = null;
                 for(int c = for3Index; c > b; c--) {
-                    if (preCval != null && nums[c] == preCval) {
-                        continue;
-                    }
 
+                    //找到命中了
                     if (nums[a] + nums[b] + nums[c] == 0) {
+
+                        //过滤掉重复的
+                        boolean repeated = false;
+                        if(!result.isEmpty()) {
+                            for(int d = result.size()-1; d >=0 ;d--) {
+                                if(result.get(d).get(0) != nums[a]) {
+                                    break;
+                                }
+
+                                if(result.get(d).get(1) == nums[b] && result.get(d).get(2) == nums[c]) {
+                                    repeated = true;
+                                }
+                            }
+                        }
+                        if(repeated) {
+                            continue;
+                        }
+                        
+                        //没有重复的，yes!!!
                         result.add(Arrays.asList(nums[a], nums[b], nums[c]));
-                        preBval = nums[b];
-                        preCval = nums[c];
+                        for3Index = c;
                         break;
                     }
-
-
                 }
-            
-                preBval = nums[b];
             }
         }
 
-        return null;
+        return result;
     }
 }
