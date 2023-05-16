febe1c6d0ae3c777ded0660987b0aedb12ec2e6b
shaogz <shaogz@bokecc.com>
2023-05-17 01:31:29 +0800

    tmp2

--- src/com/brew/home/leetcode/LeetCode32.java
+++ src/com/brew/home/leetcode/LeetCode32.java
@@ -1,6 +1,7 @@
 package com.brew.home.leetcode;
 
-
+import java.util.Deque;
+import java.util.LinkedList;
 
 /**
  *
@@ -8,8 +9,11 @@ package com.brew.home.leetcode;
 public class LeetCode32 {
 
     public static void main(String[] args) {
+        //!!! 没做出来。
+        
         //32. 最长有效括号
         //给你一个只包含 '(' 和 ')' 的字符串，找出最长有效（格式正确且连续）括号子串的长度。
+        //格式正确,指的是：有一个左便对应着一个右，对应的：((())) 这也叫格式正确
 
         //输入：s = "(()"
         //输出：2
@@ -21,28 +25,42 @@ public class LeetCode32 {
 
         //输入：s = ""
         //输出：0
-        System.out.println(longestValidParentheses("(()") == 2);
-        System.out.println(longestValidParentheses(")()())") == 4);
-        System.out.println(longestValidParentheses("") == 0);
 
-        System.out.println(longestValidParentheses(")())))(())()(())") == 8);
+        // == 2
+        System.out.println(longestValidParentheses("(()"));
+        // == 4
+        System.out.println(longestValidParentheses(")()())"));
+        // == 0
+        System.out.println(longestValidParentheses(""));
+        // == 6
+        System.out.println(longestValidParentheses(")())))(())()(())"));
     }
 
     public static int longestValidParentheses(String s) {
+        Deque<Character> stack = new LinkedList<>();
+        int res = 0;
+        int maxTmp = 0;
+        int max = 0;
+        for(int i = 0; i<s.length(); i++) {
+            char cur = s.charAt(i);
+            if (cur == '(') {
+                stack.push(cur);
+            } else {
+                if (stack.isEmpty()) {
+                    res = 0;
+                    continue;
+                } else{
+                    stack.pop();
+                    res+=2;
+                    maxTmp = Math.max(res, maxTmp);
 
-        int hit = 0;
-        char[] a = s.toCharArray();
-        for (int i = 0, j = 1; j < a.length; i++, j++) {
-            char l = a[i];
-            char r = a[j];
-
-            if(l == '(' && r == ')') {
-                //命中了就要往前挪两步
-                i++;j++;
-                hit++;
+                    if(stack.isEmpty()) {
+                        max = maxTmp;
+                    }
+                }
             }
         }
-        return hit*2;
+        return max;
     }
 
 }
