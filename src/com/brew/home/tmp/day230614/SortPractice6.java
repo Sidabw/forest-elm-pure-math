f9a86dd183a5e31c818cfc610ec66bb83f18cd15
shaogz <shaogz@bokecc.com>
2023-06-15 01:10:52 +0800

    radix sort

--- src/com/brew/home/tmp/day230614/SortPractice6.java
+++ src/com/brew/home/tmp/day230614/SortPractice6.java
@@ -12,15 +12,34 @@ public class SortPractice6 {
     private static void radixSort(int[] arr) {
         int max = getMax(arr);
         for (int place = 1; max/place>0;place*=10) {
-            courtingSort(arr, arr.length, place);
+            courtingSort(arr, place);
         }
 
     }
 
-    private static void courtingSort(int[] arr, int n, int place) {
-        //...
-    }
+    private static void courtingSort(int[] arr, int place) {
+        //part-1
+        int[] tmp = new int[10];
+        for(int i = 0; i < arr.length; i++) {
+            tmp[(arr[i]/10)%10]++;
+        }
 
+        //part-2
+        for(int i = 1; i < tmp.length; i++) {
+            tmp[i]+=tmp[i-1];
+        }
+
+        //part-3
+        int[] sorted = new int[arr.length];
+        for (int i = arr.length - 1; i >= 0; i--) {
+            sorted[tmp[(arr[i] / place) % 10] - 1] = arr[i];
+            tmp[(arr[i] / place) % 10]--;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sorted[i];
+        }
+    }
     
 
     private static int getMax(int[] arr) {
