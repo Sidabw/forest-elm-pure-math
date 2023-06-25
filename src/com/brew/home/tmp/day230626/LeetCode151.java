69324d8c6d5bbc3448f0f4a1430279727541e765
shaogz <shaogz@bokecc.com>
2023-06-26 01:39:37 +0800

    upd

---
+++ src/com/brew/home/tmp/day230626/LeetCode151.java
@@ -0,0 +1,11 @@
+package com.brew.home.tmp.day230626;
+
+public class LeetCode151 {
+    
+    public static void main(String[] args) {
+        //https://leetcode.cn/problems/reverse-words-in-a-string/
+
+        //双端队列的解法比较有意思
+        //先占坑，明天再解
+    }
+}
---
+++ src/com/brew/home/tmp/day230626/LeetCode344.java
@@ -0,0 +1,20 @@
+package com.brew.home.tmp.day230626;
+
+public class LeetCode344 {
+    
+    public static void main(String[] args) {
+        //https://leetcode.cn/problems/reverse-string/
+
+
+    }
+
+    public static void reverseString(char[] s) {
+        //没什么好玩的解法，就是最笨的双指针就可以了
+        for(int i = 0, j = s.length-1; i < j; i++, j--) {
+            char tmp = s[i];
+            s[i] = s[j];
+            s[j] = tmp;
+        }
+
+    }
+}
