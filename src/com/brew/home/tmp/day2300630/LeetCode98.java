9dd0ef8479d9e70ad003158f862b93551e9f69e8
shaogz <shaogz@bokecc.com>
2023-07-03 07:31:58 +0800

    1

--- src/com/brew/home/tmp/day2300630/LeetCode98.java
+++ src/com/brew/home/tmp/day2300630/LeetCode98.java
@@ -1,11 +1,14 @@
 package com.brew.home.tmp.day2300630;
 
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class LeetCode98 {
     
     public static void main(String[] args) {
         TreeNode r = TreeNode.build1();
         System.out.println(new LeetCode98().isValidBST(r));
+        System.out.println(new LeetCode98().isValidBST2(r));
     }
 
     public boolean isValidBST(TreeNode root) {
@@ -21,6 +24,34 @@ public class LeetCode98 {
             return false;
         }
         return isValidBST(node.left, lower, node.val) && isValidBST(node.right, node.val, upper);
+    }
+
+    public boolean isValidBST2(TreeNode root) {
+        return postR(root);
+    }
+
+    Queue<Integer> oneElQueue = new LinkedList<>();
+    private boolean postR(TreeNode node) {
+        if(node == null) {
+            return true;
+        }
+
+        boolean res1 = postR(node.left);
+        if(!res1) {
+            return res1;
+        }
+
+        if(oneElQueue.isEmpty() || oneElQueue.poll() < node.val) {
+            oneElQueue.offer(node.val);
+        } else {
+            return false;
+        }
+
+        boolean res2 = postR(node.right);
+        if(!res2) {
+            return res2;
+        }
+        return true;
     }
 }
 
