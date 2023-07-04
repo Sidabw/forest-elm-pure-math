b2b0c88b5a6d4b785f89c0fefe7b9bbd69b3953a
shaogz <shaogz@bokecc.com>
2023-07-05 00:32:15 +0800

    36.有效的数独

--- src/com/brew/home/tmp/day230704/LeetCode36.java
+++ src/com/brew/home/tmp/day230704/LeetCode36.java
@@ -5,17 +5,15 @@ import java.util.Arrays;
 public class LeetCode36 {
     
     public static void main(String[] args) {
-        
+        //有效的数独，题解：
+        //https://leetcode.cn/problems/valid-sudoku/solutions/1001859/you-xiao-de-shu-du-by-leetcode-solution-50m6/
+
+        //对于int[][][] subboxes部分，可以理解为一个黑框包起来的就是一个元素，所以是int[3][3]的二维数组，但每个元素实际是9个值，所以变成了int[3][3][9]
     }
 
     public boolean isValidSudoku(char[][] board) {
 
-        for(int i = 0 ; i < board.length; i++) {
-
-            char[] curRow = board[i];
-            
-        }
-
+        int[][][] subboxes = new int[3][3][9];
         for(int i = 0 ; i < 9; i++) {
             int[] tmp = new int[9];//对应下标[0,8]
             Arrays.fill(tmp, 0);
@@ -35,13 +33,16 @@ public class LeetCode36 {
                 if(index2 != -1 && ++tmp2[index2] > 1) {
                     return false;
                 }
+
+                if (index != -1 && ++subboxes[i / 3][j / 3][index] > 1) {
+                    return false;
+                }
             }
         }
 
         //最后没做...
-        //https://leetcode.cn/problems/valid-sudoku/solutions/1001859/you-xiao-de-shu-du-by-leetcode-solution-50m6/
-        return false;
-        //board[1][1]，board[1][2]，board[1][3]
+        
+        return true;
     }
 
     private int getIntVal(char c) {
